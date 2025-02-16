063e9dac2e85b055c1a79e5b9e0e4b5b22a42e6d
ZnowBaka <Snorrebr@gmail.com>
2025-02-16 23:39:26 +0100

    Login half done

--- src/main/java/com/example/tidrejseagenturet/FXController.java
+++ src/main/java/com/example/tidrejseagenturet/FXController.java
@@ -1,8 +1,9 @@
 package com.example.tidrejseagenturet;
 
 
+import com.example.tidrejseagenturet.Controller.CustomerController;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 
@@ -18,7 +19,7 @@ public class FXController {
     private Parent root;
     private Stage stage;
     private Scene scene;
-
+    CustomerController customerController = new CustomerController();
 
     //region WelcomeScreen Ids & Actions
     @FXML
@@ -31,32 +32,102 @@ public class FXController {
     }
 
     //endregion
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private Button welcomeScreenBTN;
+    @FXML
+    private Button reserveBTN;
+    @FXML
+    private Button loginBTN;
+
+
+    @FXML
+    private TextField firstNameTF;
+    @FXML
+    private TextField lastNameTF;
+    @FXML
+    private TextField phoneTF;
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TableView selectedTableView;
+    @FXML
+    private TableColumn selectedTimeMachineColumn;
+    @FXML
+    private TableColumn selectedGuideColumn;
+    @FXML
+    private TableColumn selectedSteatColumn;
+    @FXML
+    private TableView possibleTableView;
+    @FXML
+    private TableColumn possibleTimeMachineColumn;
+    @FXML
+    private TableColumn possibleGuideColumn;
+    @FXML
+    private TableColumn possibleSteatColumn;
+
+
+    @FXML
+    private ChoiceBox<Integer> seatsChoiceBox;
+    @FXML
+    private ChoiceBox<String> timeRangeChoiceBox;
 
 
 
-
-
-    //region BookingScene Ids & Actions
     @FXML
     public void onReserveBTN(ActionEvent event) throws IOException {
 
-
     }
+
+    @FXML
+    public void switchToConfirmationScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("ConfirmationScene.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void switchToLoginScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scene02.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onLoginBTN(ActionEvent event) throws IOException {
+
+        if(phoneTF.getText().length() < 8 || phoneTF.getText().length() > 8) {
+            System.out.println("Phone number is unavailable");
+        } else if (emailField.getText().contains("@gmail.com") || emailField.getText().contains("@yahoo.com") || emailField.getText().contains("@hotmail.com")) {
+            customerController.newCustomer(firstNameTF.getText().toString(), lastNameTF.getText().toString(), emailField.getText().toString(), phoneTF.getText().toString());
+        } else {
+            System.out.println("Email is unavailable");
+        }
+    }
+
+/*
+    @FXML
+    public void switchToScene03(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scene03.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+*/
+
+    //region BookingScene Ids & Actions
     //endregion
 
 
-
-
-
-
-
-
-
-
-
-
-
     //region TestScenes
+    /*
     @FXML
     public void switchToScene1(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Scene01.fxml"));
@@ -74,6 +145,7 @@ public class FXController {
         stage.setScene(scene);
         stage.show();
     }
+     */
     //endregion
 
 
